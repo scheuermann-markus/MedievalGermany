8f24414ecf6bef17e6cb5c260b1cb1b2c6c39f52
Markus <scheuermann-markus@gmx.de>
2024-06-15 15:53:36 +0200

    Deutschlandkarte-Component hinzugefügt

---
+++ MedievalGermany/wwwroot/js/deutschlandkarte.js
@@ -0,0 +1,32 @@
+﻿export async function InitMap() {
+    var dk = document.getElementById("deutschland-karte");
+
+    var map = L.map(dk, {
+        zoomSnap: 0.1,
+    }).setView([51.3, 9.3], 5.5);
+
+    // Kartenmaterial laden
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        minZoom: 5,
+        maxZoom: 16,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Initialisierte Karte dem Element hinzufügen, damit wir im Code weiter unten darauf zugreifen können.
+    dk.MapInstance = map;
+}
+
+export async function CreateMarker() {
+    var dk = document.getElementById("deutschland-karte");
+    if (dk === null) {
+        return;
+    }
+
+    var map = dk.MapInstance;
+    if (map == null) {
+        return;
+    }
+
+
+}
+
