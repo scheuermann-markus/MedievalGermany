05fa7de79742b06c0d892911dbf2c6eae6c0d5e8
VPS Server <Administrator@VMD143246>
2024-06-23 10:43:24 +0200

    Gitignore angepasst und bisschen aufgeräumt

--- MedievalGermany/Application/Services/CastleService.cs
+++ MedievalGermany/Application/Services/CastleService.cs
@@ -39,54 +39,5 @@ namespace MedievalGermany.Application.Services
 
             return result;
         }
-
-
-        //public static IEnumerable<Castle> Data = new List<Castle>
-        //{
-        //    new Castle
-        //    {
-        //        Id = "Castle/" + Guid.NewGuid().ToString(),
-        //        Name = "Schloss Neuschwanstein",
-        //        Eroeffnet = 1884,
-        //        Geolocation = new Geolocation
-        //        {
-        //            Latitude = 47.557732,
-        //            Longitude = 10.749646,
-        //        }
-        //    },
-        //    new Castle
-        //    {
-        //        Id = "Castle/" + Guid.NewGuid().ToString(),
-        //        Name = "Schloss Heidelberg",
-        //        Eroeffnet = 1214,
-        //        Geolocation = new Geolocation
-        //        {
-        //            Latitude = 49.41062,
-        //            Longitude = 8.715309,
-        //        }
-        //    },
-        //    new Castle
-        //    {
-        //        Id = Guid.NewGuid().ToString(),
-        //        Name = "Schloss Herrenchiemsee",
-        //        Eroeffnet = 1878,
-        //        Geolocation = new Geolocation
-        //        {
-        //            Latitude = 47.867265,
-        //            Longitude = 12.39588,
-        //        }
-        //    },
-        //    new Castle
-        //    {
-        //        Id = Guid.NewGuid().ToString(),
-        //        Name = "Schloss Schwerin",
-        //        Eroeffnet = 1857,
-        //        Geolocation = new Geolocation
-        //        {
-        //            Latitude = 53.624468,
-        //            Longitude = 11.418173,
-        //        }
-        //    },
-        //};
     }
 }
